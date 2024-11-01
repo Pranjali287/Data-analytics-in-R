6478d4a6ba1005b2ea2ce74d1141d93b5f107f2a
Pranjali Mali <pranjalia.23.beai@acharya.ac.in>
2024-11-01 08:57:47 +0530

    Add files via upload

---
+++ CSV.R
@@ -0,0 +1,17 @@
+?read.csv()
+read.table(CSV1,nea)
+dt=read.table(file.choose(),header = TRUE,sep=)
+dt
+dt1=read.csv(file.choose(),header = TRUE,skip = 1)
+dt1
+employee=data.frame("employee_id"=c(2,3,4,5,6),"year_of_joining"=c(2002,2004,2005,2006,2007),"year_of_experience"=c(4,5,6,7,2))
+employee 
+?write.csv
+write.csv(employee,"employee.csv")
+getwd()
+write.csv(employee,"employee1.csv",row.names=FALSE)
+getwd()
+dt2=read.table(file.choose(),header = )
+?readline
+read=readline(file.choose())
+read
